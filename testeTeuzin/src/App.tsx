c004a415923995caaf371227dd16bafb1a0bbaea
matheus <matheussfilipej@gmail.com>
2026-04-30 12:29:19 -0300

    att

---
+++ testeTeuzin/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div>Peitinhooooooo</div>
+    </>
+  )
+}
+
+export default App
---
+++ testeTeuzin/src/cuzin.tsx
@@ -0,0 +1,10 @@
+
+
+export function Cuzinnnn() {
+
+    return (
+        <div>
+            Cuzinhoooooooooo
+        </div>
+    )
+}
