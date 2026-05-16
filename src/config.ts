9a1bc472d52bdb6db9d3706fab23f423765723fe
[dyad] <teuzintw@gmail.com>
2026-05-16 11:58:45 -0300

    [dyad] Atualizando nomes do banco e coleção via config.ts - wrote 1 file(s)

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const API_BASE_URL = "http://localhost:3333";
 
 export const API_HEADERS = {
   "Content-Type": "application/json",
-  "x-base": "financas_db",
-  "x-collection": "transacoes"
+  "x-base": "api_financas",
+  "x-collection": "teuzin_financas"
 };
